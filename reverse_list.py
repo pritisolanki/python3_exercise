45732bbd3a88326c5e90ad34e5d25710e6918415
Priti Solanki <pritisolanki@Pritis-MacBook-Pro.local>
2022-04-28 18:34:05 +0530

    explored reverse list in python3

---
+++ reverse_list.py
@@ -0,0 +1,17 @@
+#Exploring Range function
+
+list_item = range(1,11)
+#list_item.reverse()
+
+#AttributeError: 'range' object has no attribute 'reverse' 🤔
+#In Python2, range returns a list.
+#In Python3, range returns a range object. (everything is object)
+
+#Solution : Using reverse
+list_item = list(range(1,11))
+list_item.reverse()
+print(list_item)
+
+#Solution : Use slicing
+reversed_list = list_item[len(list_item)-1::-1]
+print(reversed_list)
--- split.py
+++ split.py
@@ -1,10 +1,10 @@
-#split string example
+#split string 
 
 input_string = 'lorem,porem,something'
 string_list = input_string.split(',')
 print(string_list)
 
 #how many splits ?
-input_string2 = "lorem,porem,something,interesting,continued"
+input_string2 = "lorem,porem,something,exciting,continued"
 string2_list = input_string2.split(',',maxsplit=2)
 print(string2_list)
