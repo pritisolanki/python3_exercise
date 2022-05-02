e8e8e9a9230879c0816d7d23d6b4a2cfcc8aa842
Priti Solanki <pritisolanki@Pritis-MacBook-Pro.local>
2022-05-02 18:06:48 +0530

    change in name

--- lambda_function.py
+++ lambda_function.py
@@ -6,14 +6,17 @@ from numpy import sort
 #create a lambda function that sorts the list in descending order
 #case 1
 lst = [23, 45, 56, 78, 99, 35]
-sorted_list = lambda lst: sorted(lst, reverse=True)
-print(sorted_list(lst))
+reverse_list = lambda lst: sorted(lst, reverse=True)
+print(reverse_list(lst))
 
 #case 2
 lst = sorted(lst, key=lambda x: x, reverse=True)
 print(lst)
 
+#what is the pythonic way of writing lambda function case 1 OR case 2?
+
 #perform list join using lambda
 my_list = ['lorem', 'porem','sorem']
 joinner = ' '.join(map(lambda x: x, my_list))
 print(joinner)
+
