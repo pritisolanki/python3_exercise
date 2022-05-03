ef545a4a5c6e7d031dffab2744dcb6cedde0c8f3
Priti Solanki <pritisolanki@Pritis-MacBook-Pro.local>
2022-05-03 14:38:13 +0530

    practice lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,9 @@
 # Lambda function exercise
 #create a lambda function that sorts the list in descending order - Priti
 #case 1
+from numpy import intp, number
+
+
 lst = [23, 45, 56, 78, 99, 35]
 reverse_list = lambda lst: sorted(lst, reverse=True)
 print(reverse_list(lst))
@@ -22,6 +25,7 @@ subject_mark = [('English', 88), ('Science', 90), ('Maths', 97), ('Social scienc
 subject_mark.sort(key =lambda x: x[1])
 print(subject_mark)
 
+#sort cars based on model
 cars = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
 cars.sort(key=lambda x : int(x['model']))
 print(cars)
@@ -30,3 +34,10 @@ lst=["otter", "whale", "goose", "chipmunk", "fox", "sheep", "rabbit", "marten"]
 #Using sorted() function and lambda sort the words in the list based on their second letter from a to z.
 lst=sorted(lst,key=lambda x : x[1])
 print(lst)
+
+#Write a python program to generate a lambda function to check whether a given string is a number or not.
+result = lambda x: True if type(x) is str or type(x) is int else False
+print(result('hello'))
+print(result(11))
+print(result(1.2))
+
