a448a90d9c82931df3bc04ac7e50b6de17f8f454
Priti Solanki <pritisolanki@Pritis-MacBook-Pro.local>
2022-05-09 11:21:58 +0530

    exploring print formatting

--- print_exercise.py
+++ print_exercise.py
@@ -7,6 +7,7 @@ Ajay            Mumbai                  15      Coding
 Vijay           Ahmedabad               20      Singing, Dancing
 Varun           Shimla                  21      Programming
 '''
+import csv
 
 heading=['Name','Cities','Age','Hobbies']
 cities = ['Bangalore','Delhi','Mumbai','Ahmedabad','Shimla','Udaipur']
@@ -15,7 +16,13 @@ age = [14,12,15,20,21,13]
 likes = ['Hiking, Swimming', 'Walking','Coding','Singing, Dancing','Programming']
 file_data = list(zip(names,cities,age,likes))
 
+csv_file_handle = open("student.csv","w")
+writer = csv.writer(csv_file_handle)
+writer.writerow(heading)
+
 print(f'{heading[0].ljust(10)}\t{heading[1].ljust(20)}\t{heading[2].ljust(3)}\t{heading[3]}')
 for row in file_data: 
     print(f'{row[0].ljust(10)}\t{row[1].ljust(20)}\t{str(row[2]).ljust(3)}\t{row[3]}')
+    writer.writerow(row)
 
+csv_file_handle.close()
