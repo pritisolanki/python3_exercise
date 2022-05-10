a96588378a7fa0cc272cc044e42beec10932239e
pritisolanki <pritiatwork@local.com>
2022-05-10 22:23:25 +0530

    updates in code

--- print_exercise.py
+++ print_exercise.py
@@ -1,19 +1,12 @@
 #exploring print - explore padding formatting
-'''
-Name            Cities                  Age     Hobbies
-Raj             Bangalore               14      Hiking, Swimming
-Simran          Delhi                   12      Walking
-Ajay            Mumbai                  15      Coding
-Vijay           Ahmedabad               20      Singing, Dancing
-Varun           Shimla                  21      Programming
-'''
+
 import csv
 
 heading=['Name','Cities','Age','Hobbies']
-cities = ['Bangalore','Delhi','Mumbai','Ahmedabad','Shimla','Udaipur']
-names = ['Raj','Simran','Ajay','Vijay','Varun','Arjun']
-age = [14,12,15,20,21,13]
-likes = ['Hiking, Swimming', 'Walking','Coding','Singing, Dancing','Programming']
+cities = ['Bangalore','Delhi','Mumbai','Ahmedabad','Shimla','Udaipur','Agra','Delhi']
+names = ['Raj','Simran','Ajay','Vijay','Varun','Arjun','Rani','Anu']
+age = [14,12,15,20,21,13,21,11]
+likes = ['Hiking, Swimming', 'Walking','Coding','Singing, Dancing','Programming','Dancing','Hiking','Coding']
 file_data = list(zip(names,cities,age,likes))
 
 csv_file_handle = open("student.csv","w")
@@ -21,7 +14,7 @@ writer = csv.writer(csv_file_handle)
 writer.writerow(heading)
 
 print(f'{heading[0].ljust(10)}\t{heading[1].ljust(20)}\t{heading[2].ljust(3)}\t{heading[3]}')
-for row in file_data: 
+for row in zip(names,cities,age,likes): 
     print(f'{row[0].ljust(10)}\t{row[1].ljust(20)}\t{str(row[2]).ljust(3)}\t{row[3]}')
     writer.writerow(row)
 
