ac850d83abda9da8fc68f580d78caa5be3478556
pritisolanki <pritiatwork@gmail.com>
2022-05-23 21:43:13 +0530

    arrow function

--- utilities/arrow_date.py
+++ utilities/arrow_date.py
@@ -1,5 +1,5 @@
 import arrow
-
+import datetime
 #format
 print(f'current time in PST: { arrow.now("US/Pacific").format("MMMM DD, YYYY HH:mm A")}');
 print(f'current time in EST: { arrow.now("America/New_York").format("MMMM DD, YYYY HH:mm A")}');
@@ -14,3 +14,15 @@ flight_schedule_date = arrow.now("America/New_York")
 flight_landing_date = flight_schedule_date.shift(hours=28).to('+05:30')
 
 print(f'flight scheduled departure is {flight_schedule_date.format("DD,MMM HH:mm A ZZZ")} and expected arrival at destination is {flight_landing_date.format("DD,MMM HH:mm A ZZ")}')
+
+
+startdate = arrow.now("America/New_York")
+enddate = startdate.shift(hours=23)
+print(f'We move at: {startdate.format("DD,MMM HH:mm A ZZZ")} and will arrival at destination at {enddate.format("DD,MMM HH:mm A ZZZ")}')
+assembledate = startdate.shift(minutes=120)
+print(f'Team gathers at {startdate.humanize(assembledate)} at school gate')
+
+#Output
+
+#We move at: 23,May 12:10 PM EDT and will arrival at destination at 24,May 11:10 AM EDT
+#Team gathers at 2 hours ago at school gate
