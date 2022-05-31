e64c3db3cc68ffb2f46617316103d3d563d02748
pritisolanki <pritiatwork@gmail.com>
2022-05-31 09:12:07 +0530

    Task: Understanding numpy slicing

---
+++ datascience/numpy_slicing.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Understanding the indexing and slicing in matrix
+data = np.array([[1, 2,9], [3, 4,8], [5, 6,7],[4,5,6]])
+#pick three rows
+print(data[0:3])
+'''
+[[1 2 9]
+ [3 4 8]
+ [5 6 7]]
+'''
+#create rows by picking 1 index
+print(data[0:3,1])
+'''
+[2 4 6]
+'''
+#create matrix by picking cols index from 0 to 2
+print(data[0:3, 0:2])
+#output
+'''
+[[1 2]
+ [3 4]
+ [5 6]]
+'''
