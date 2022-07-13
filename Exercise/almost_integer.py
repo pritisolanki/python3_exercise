e0d87f19ac31fa2f54dadda5b4e38407f6bd605c
pritisolanki <pritiatwork@gmail.com>
2022-07-13 13:43:09 -0400

    Task: Refactor

---
+++ Exercise/almost_integer.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+import math
+
+
+def h(n):
+    "Simple, reduced precision calculation"
+    return math.factorial(n) / (2 * math.log(2) ** (n + 1))
+
+
+def h2(n):
+    "Extended precision Hickerson function"
+    return Decimal(math.factorial(n)) / (2 * Decimal(2).ln() ** (n + 1))
+
+
+if __name__ == "__main__":
+    for n in range(18):
+        x = h2(n)
+        norm = str(x.normalize())
+        print(norm)
+        almostinteger = (
+            " Nearly integer"
+            if "E" not in norm and (".0" in norm or ".9" in norm)
+            else " NOT nearly integer!"
+        )
+        print("n:%2i h:%s%s" % (n, norm, almostinteger))
